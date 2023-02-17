1d546726b3e3972de4469ec04099c2b18cde2687
ElieZoetaba <zoetabaelie@gmail.com>
2023-02-17 23:12:19 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+/*Menu btn toggle*/
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+/*Search btn toggle*/
+menu.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+document.querySelector('#search-icon').onclick = () => {
+    document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () => {
+    document.querySelector('#search-form').classList.remove('active');
+}
+
+/**/
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
